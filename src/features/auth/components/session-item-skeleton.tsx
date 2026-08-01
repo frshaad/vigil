c1f692a24e0daec4ed2d53bf20229125221c1d21
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2026-08-01 13:25:40 +0330

    Update session item skeleton styling

--- src/features/auth/components/session-item-skeleton.tsx
+++ src/features/auth/components/session-item-skeleton.tsx
@@ -3,16 +3,16 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function SessionItemSkeleton() {
   return (
-    <Item variant="outline">
+    <Item variant="outline" className="items-baseline">
       <ItemMedia variant="icon">
-        <Skeleton className="size-20" />
+        <Skeleton className="size-4" />
       </ItemMedia>
 
       <ItemContent>
-        <Skeleton className="h-6 w-60" />
-        <Skeleton className="h-4 w-60" />
-        <Skeleton className="h-4 w-60" />
-        <Skeleton className="h-4 w-60" />
+        <Skeleton className="h-5 w-64" />
+        <Skeleton className="h-5 w-60" />
+        <Skeleton className="h-5 w-15" />
+        <Skeleton className="h-5 w-40" />
       </ItemContent>
     </Item>
   );
